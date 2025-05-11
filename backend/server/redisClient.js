b1656f57033c4fe461e557559334ad2f010a6900
Aditya Thakur <at8482937@gmail.com>
2025-05-11 17:10:53 +0530

    deploying the app

--- backend/server/redisClient.js
+++ backend/server/redisClient.js
@@ -2,6 +2,7 @@ const redis = require('redis');
 require('dotenv').config();
 
 const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+
 redisClient.connect().catch(console.error);
 
 module.exports = redisClient;
